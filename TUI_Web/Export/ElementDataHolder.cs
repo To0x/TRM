5329c3b54bfd45233a76c9a3f80814798734940e
Tom Schubert <t.schubert1988@web.de>
2016-12-30 06:59:34 +0100

    added File which holds the content of the webPage (eg. Loren-Ipsum text or image-file)

---
+++ TUI_Web/Export/ElementDataHolder.cs
@@ -0,0 +1,86 @@
+﻿using System;
+namespace TUI_Web
+{
+	public static class ElementDataHolder
+	{
+		private static string imagePath = "/img.jpg";
+		private static string textContent = "Lorem Ipsum";
+		private static string topicContent = "Topic";
+
+		/*
+		private static string getHtmlData(ElementTypes type)
+		{
+
+			string data = null;
+			switch (type)
+			{
+				case ElementTypes.None:
+					data = "<div></div>";
+					break;
+
+				case ElementTypes.Graphic:
+					data = "<img id=%d src=%s />";
+					break;
+
+				case ElementTypes.Text:
+					data = "<div>%s</div>";
+					break;
+
+				case ElementTypes.Topic:
+					data = "<h1>%s</h1>";
+					break;
+
+				default:
+					data = String.Empty;
+					break;
+			}
+
+			return data;
+		}
+		*/
+
+		public static string getImageSourcePath()
+		{
+			return imagePath;
+		}
+
+		public static string getTopicContent()
+		{
+			return topicContent;
+		}
+
+		public static string getTextContent()
+		{
+			return textContent;
+		}
+
+		/*
+		public static string getHtmlContent(ElementTypes type)
+		{
+			string data = null;
+			switch (type)
+			{
+				case ElementTypes.Graphic:
+					data =  imagePath;
+					break;
+
+				case ElementTypes.Text:
+					data = textContent;
+					break;
+
+				case ElementTypes.Topic:
+					data = topicContent;
+					break;
+
+				case ElementTypes.None:
+					
+				default:
+					data = String.Empty;
+					break;
+			}
+
+			return data;
+		}
+		*/
+	}
+}
